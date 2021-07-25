f04016343b2c32db63baf619ebfa9f993320f7f9
adityaparvatepatil <85201656+adityaparvatepatil@users.noreply.github.com>
2021-07-25 11:24:32 -0400

    Changes

--- Prework/Prework/ViewController.swift
+++ Prework/Prework/ViewController.swift
@@ -9,21 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
-
-    @IBOutlet weak var tipAmountLabel: UILabel!
-    @IBOutlet weak var billAmountTextField: UITextField!
-    
-    @IBOutlet weak var totalLabel: UILabel!
-    
-    
-    @IBOutlet weak var tipControl: UISegmentedControl!
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
+    @IBOutlet weak var billAmountTextField: UITextField!
+    
+    
+    @IBOutlet weak var totalLabel: UILabel!
+
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    
+   
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+
+    
     
     @IBAction func calculateTip(_ sender: Any) {
         //Extracting bill from input bill
